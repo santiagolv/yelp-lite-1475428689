308ed7e38bea1cf62068ce97adafa074d75d6c97
Raghu Betina <raghu@firstdraft.com>
2016-10-02 17:20:47 +0000

    Generate review_replies resource with starter:resource

---
+++ db/migrate/20161002172047_create_review_replies.rb
@@ -0,0 +1,11 @@
+class CreateReviewReplies < ActiveRecord::Migration
+  def change
+    create_table :review_replies do |t|
+      t.text :reply_content
+      t.integer :review_id
+
+      t.timestamps
+
+    end
+  end
+end
